b24f31fa0549572207031255244a3709f941f130
Caleb Crouch <cecrouch01@gmail.com>
2023-09-01 10:02:31 -0400

    addition of server

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+const routes = require('./controllers/index');
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: '',
+    cookie: {
+        maxAge: 60 * 1000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({helpers});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => 
+    console.log(`Server running on port ${PORT}. To visit site go to http://localhost:${PORT}`))
+})
